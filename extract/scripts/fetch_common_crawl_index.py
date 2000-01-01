dbfd3ea1361d79985550808709663118ce267982
Vintage Bot Dashboard <bot-dashboard@localhost>
2000-01-01 00:00:00 +0000

    bot-dashboard: Common Crawl Index dataset ingestion

---
+++ extract/scripts/fetch_common_crawl_index.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python3
+"""Fetch bounded domain snapshots from the latest Common Crawl CDX index.
+
+The collection metadata endpoint is authoritative for both crawl selection and the
+crawl-specific index URL.  A watchlist run resolves that metadata once, then uses the
+same crawl for every domain so records from one run cannot straddle crawl releases.
+Output is normalized newline-delimited JSON suitable for automatic RAW schema
+inference.
+
+Stdlib only.
+"""
+
+from __future__ import annotations
+
+import argparse
+from datetime import datetime, timezone
+import json
+import os
+import re
+import sys
+import urllib.parse
+import urllib.request
+
+SOURCE = "common_crawl_index"
+COLLINFO_URL = "https://index.commoncrawl.org/collinfo.json"
+USER_AGENT = os.environ.get("EXTRACT_USER_AGENT") or (
+    "vintage-data/0.1 (+https://github.com/cdlethem/vintage-data)"
+)
+DEFAULT_TIMEOUT = 30.0
+DEFAULT_PAGE_SIZE = 1
+DEFAULT_MAX_PAGES = 5
+MAX_RESPONSE_BYTES = 32 * 1024 * 1024
+CURATED_DOMAINS = (
+    "bbc.com",
+    "cdc.gov",
+    "github.com",
+    "noaa.gov",
+    "openai.com",
+    "un.org",
+    "wikipedia.org",
+    "worldbank.org",
+)
+
+_CRAWL_ID_RE = re.compile(r"^CC-MAIN-\d{4}-\d{2}$")
+_TIMESTAMP_RE = re.compile(r"^\d{14}$")
+
+
+class CommonCrawlError(RuntimeError):
+    """The Common Crawl response did not satisfy the extractor contract."""
+
+
+def _request(url: str) -> urllib.request.Request:
+    return urllib.request.Request(
+        url,
+        headers={"Accept": "application/json", "User-Agent": USER_AGENT},
+    )
+
+
+def _read_response(url: str, timeout: float) -> bytes:
+    with urllib.request.urlopen(_request(url), timeout=timeout) as response:
+        payload = response.read(MAX_RESPONSE_BYTES + 1)
+    if len(payload) > MAX_RESPONSE_BYTES:
+        raise CommonCrawlError(f"response from {url!r} exceeded {MAX_RESPONSE_BYTES} bytes")
+    return payload
+
+
+def _read_json(url: str, timeout: float, *, context: str):
+    payload = _read_response(url, timeout)
+    try:
+        return json.loads(payload)
+    except (UnicodeDecodeError, json.JSONDecodeError) as exc:
+        raise CommonCrawlError(f"{context} returned malformed JSON") from exc
+
+
+def _with_query(url: str, parameters: dict[str, object]) -> str:
+    parts = urllib.parse.urlsplit(url)
+    query = urllib.parse.parse_qsl(parts.query, keep_blank_values=True)
+    query.extend((key, str(value)) for key, value in parameters.items())
+    return urllib.parse.urlunsplit(
+        (parts.scheme, parts.netloc, parts.path, urllib.parse.urlencode(query), parts.fragment)
+    )
+
+
+def _validate_positive(name: str, value: float) -> None:
+    if isinstance(value, bool) or value <= 0:
+        raise ValueError(f"{name} must be positive")
+
+
+def discover_crawls(
+    collinfo_url: str = COLLINFO_URL,
+    *,
+    timeout: float = DEFAULT_TIMEOUT,
+) -> list[dict[str, str]]:
+    """Return validated crawl IDs and their crawl-specific CDX endpoints."""
+    _validate_positive("timeout", timeout)
+    document = _read_json(collinfo_url, float(timeout), context="collection metadata")
+    if not isinstance(document, list):
+        raise CommonCrawlError("collection metadata must be a JSON list")
+
+    crawls: list[dict[str, str]] = []
+    seen: set[str] = set()
+    for position, item in enumerate(document):
+        if not isinstance(item, dict):
+            raise CommonCrawlError(f"collection metadata item {position} must be an object")
+        crawl_id = item.get("id")
+        index_url = item.get("cdx-api")
+        if not isinstance(crawl_id, str) or not _CRAWL_ID_RE.fullmatch(crawl_id):
+            raise CommonCrawlError(f"collection metadata item {position} has an invalid id")
+        if not isinstance(index_url, str) or not index_url.startswith(("http://", "https://")):
+            raise CommonCrawlError(
+                f"collection metadata item {position} has an invalid cdx-api URL"
+            )
+        if crawl_id in seen:
+            raise CommonCrawlError(f"collection metadata repeats crawl {crawl_id!r}")
+        seen.add(crawl_id)
+        crawls.append({"id": crawl_id, "cdx-api": index_url})
+    if not crawls:
+        raise CommonCrawlError("collection metadata contains no crawls")
+    return crawls
+
+
+def select_crawl(
+    crawls: list[dict[str, str]], crawl_id: str | None = None
+) -> dict[str, str]:
+    """Select an explicit crawl, or the newest crawl by Common Crawl ID."""
+    if crawl_id is not None:
+        for crawl in crawls:
+            if crawl["id"] == crawl_id:
+                return crawl
+        raise CommonCrawlError(f"crawl {crawl_id!r} is not present in collection metadata")
+    return max(crawls, key=lambda crawl: crawl["id"])
+
+
+def resolve_crawl(
+    crawl_id: str | None = None,
+    *,
+    collinfo_url: str = COLLINFO_URL,
+    timeout: float = DEFAULT_TIMEOUT,
+) -> dict[str, str]:
+    """Resolve a crawl ID to the index URL advertised by collection metadata."""
+    return select_crawl(
+        discover_crawls(collinfo_url=collinfo_url, timeout=timeout),
+        crawl_id=crawl_id,
+    )
+
+
+def _normalize_domain(domain: str) -> str:
+    if not isinstance(domain, str):
+        raise ValueError("domain must be a string")
+    value = domain.strip().lower().rstrip(".")
+    if (
+        not value
+        or "://" in value
+        or "/" in value
+        or ":" in value
+        or value.startswith(".")
+        or value.endswith(".")
+        or ".." in value
+        or any(not (part.replace("-", "").isalnum()) for part in value.split("."))
+    ):
+        raise ValueError(f"invalid domain {domain!r}")
+    return value
+
+
+def _page_count(
+    domain: str,
+    index_url: str,
+    *,
+    page_size: int,
+    timeout: float,
+) -> int:
+    url = _with_query(
+        index_url,
+        {
+            "url": f"{domain}/*",
+            "matchType": "domain",
+            "output": "json",
+            "pageSize": page_size,
+            "showNumPages": "true",
+        },
+    )
+    document = _read_json(url, timeout, context=f"page metadata for {domain}")
+    if not isinstance(document, dict):
+        raise CommonCrawlError(f"page metadata for {domain} must be an object")
+    pages = document.get("pages")
+    if isinstance(pages, bool) or not isinstance(pages, int) or pages < 0:
+        raise CommonCrawlError(f"page metadata for {domain} has an invalid pages value")
+    return pages
+
+
+def _parse_page(payload: bytes, *, domain: str, page: int) -> list[dict]:
+    rows: list[dict] = []
+    for line_number, raw_line in enumerate(payload.splitlines(), 1):
+        if not raw_line.strip():
+            continue
+        try:
+            row = json.loads(raw_line)
+        except (UnicodeDecodeError, json.JSONDecodeError) as exc:
+            raise CommonCrawlError(
+                f"index page {page} for {domain} has malformed JSON on line {line_number}"
+            ) from exc
+        if not isinstance(row, dict):
+            raise CommonCrawlError(
+                f"index page {page} for {domain} line {line_number} must be an object"
+            )
+        rows.append(row)
+    return rows
+
+
+def _optional_string(row: dict, key: str):
+    value = row.get(key)
+    if value is None:
+        return None
+    if not isinstance(value, (str, int)) or isinstance(value, bool):
+        raise CommonCrawlError(f"index record field {key!r} must be scalar")
+    return str(value)
+
+
+def _optional_integer(row: dict, key: str):
+    value = row.get(key)
+    if value is None:
+        return None
+    if isinstance(value, bool):
+        raise CommonCrawlError(f"index record field {key!r} must be an integer")
+    try:
+        number = int(value)
+    except (TypeError, ValueError) as exc:
+        raise CommonCrawlError(f"index record field {key!r} must be an integer") from exc
+    if number < 0:
+        raise CommonCrawlError(f"index record field {key!r} must not be negative")
+    return number
+
+
+def normalize_record(
+    row: dict,
+    *,
+    crawl_id: str,
+    domain: str,
+    fetched_at: str,
+) -> dict:
+    """Map one CDX object to a stable, flat, auto-inferable record."""
+    url_key = row.get("urlkey")
+    timestamp = row.get("timestamp")
+    url = row.get("url")
+    if not isinstance(url_key, str) or not url_key:
+        raise CommonCrawlError("index record has an invalid urlkey")
+    if not isinstance(timestamp, str) or not _TIMESTAMP_RE.fullmatch(timestamp):
+        raise CommonCrawlError("index record has an invalid timestamp")
+    if not isinstance(url, str) or not url:
+        raise CommonCrawlError("index record has an invalid url")
+    try:
+        captured_at = datetime.strptime(timestamp, "%Y%m%d%H%M%S").replace(
+            tzinfo=timezone.utc
+        ).isoformat()
+    except ValueError as exc:
+        raise CommonCrawlError("index record has an invalid timestamp") from exc
+
+    return {
+        "source": SOURCE,
+        "id": f"{crawl_id}:{url_key}:{timestamp}",
+        "fetched_at": fetched_at,
+        "crawl_id": crawl_id,
+        "watched_domain": domain,
+        "url_key": url_key,
+        "timestamp": timestamp,
+        "captured_at": captured_at,
+        "url": url,
+        "mime_type": _optional_string(row, "mime"),
+        "detected_mime_type": _optional_string(row, "mime-detected"),
+        "status_code": _optional_string(row, "status"),
+        "digest": _optional_string(row, "digest"),
+        "content_length": _optional_integer(row, "length"),
+        "warc_offset": _optional_integer(row, "offset"),
+        "warc_filename": _optional_string(row, "filename"),
+        "languages": _optional_string(row, "languages"),
+        "encoding": _optional_string(row, "encoding"),
+    }
+
+
+def fetch_domain(
+    domain: str,
+    crawl: dict[str, str],
+    *,
+    page_size: int = DEFAULT_PAGE_SIZE,
+    max_pages: int = DEFAULT_MAX_PAGES,
+    timeout: float = DEFAULT_TIMEOUT,
+    fetched_at: str | None = None,
+):
+    """Yield normalized records from bounded CDX pages for one domain."""
+    _validate_positive("page_size", page_size)
+    _validate_positive("max_pages", max_pages)
+    _validate_positive("timeout", timeout)
+    normalized_domain = _normalize_domain(domain)
+    try:
+        crawl_id = crawl["id"]
+        index_url = crawl["cdx-api"]
+    except (KeyError, TypeError) as exc:
+        raise ValueError("crawl must contain id and cdx-api") from exc
+    if not isinstance(crawl_id, str) or not isinstance(index_url, str):
+        raise ValueError("crawl id and cdx-api must be strings")
+
+    observed_at = fetched_at or datetime.now(timezone.utc).isoformat()
+    pages = min(
+        _page_count(
+            normalized_domain,
+            index_url,
+            page_size=int(page_size),
+            timeout=float(timeout),
+        ),
+        int(max_pages),
+    )
+    for page in range(pages):
+        url = _with_query(
+            index_url,
+            {
+                "url": f"{normalized_domain}/*",
+                "matchType": "domain",
+                "output": "json",
+                "pageSize": int(page_size),
+                "page": page,
+            },
+        )
+        payload = _read_response(url, float(timeout))
+        for row in _parse_page(payload, domain=normalized_domain, page=page):
+            yield normalize_record(
+                row,
+                crawl_id=crawl_id,
+                domain=normalized_domain,
+                fetched_at=observed_at,
+            )
+
+
+def fetch_watchlist(
+    domains=CURATED_DOMAINS,
+    *,
+    crawl_id: str | None = None,
+    collinfo_url: str = COLLINFO_URL,
+    page_size: int = DEFAULT_PAGE_SIZE,
+    max_pages: int = DEFAULT_MAX_PAGES,
+    timeout: float = DEFAULT_TIMEOUT,
+    fetched_at: str | None = None,
+):
+    """Fetch every domain while isolating domain-specific HTTP or data failures.
+
+    Crawl discovery and selection happen exactly once.  Even an explicit ``crawl_id``
+    is resolved through ``collinfo_url`` because that metadata supplies its CDX URL.
+    """
+    _validate_positive("page_size", page_size)
+    _validate_positive("max_pages", max_pages)
+    _validate_positive("timeout", timeout)
+    normalized_domains = [_normalize_domain(domain) for domain in domains]
+    crawl = resolve_crawl(
+        crawl_id=crawl_id,
+        collinfo_url=collinfo_url,
+        timeout=float(timeout),
+    )
+    observed_at = fetched_at or datetime.now(timezone.utc).isoformat()
+    for domain in normalized_domains:
+        try:
+            yield from fetch_domain(
+                domain,
+                crawl,
+                page_size=int(page_size),
+                max_pages=int(max_pages),
+                timeout=float(timeout),
+                fetched_at=observed_at,
+            )
+        except Exception as exc:  # noqa: BLE001 - one domain must not abort the watchlist
+            print(f"{SOURCE}: skipping {domain!r}: {exc!r}", file=sys.stderr)
+
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("domains", nargs="*", help="domains to fetch; defaults to curated set")
+    parser.add_argument("--crawl-id")
+    parser.add_argument("--collinfo-url", default=COLLINFO_URL)
+    parser.add_argument("--page-size", type=int, default=DEFAULT_PAGE_SIZE)
+    parser.add_argument("--max-pages", type=int, default=DEFAULT_MAX_PAGES)
+    parser.add_argument("--timeout", type=float, default=DEFAULT_TIMEOUT)
+    return parser
+
+
+def main(argv: list[str] | None = None) -> None:
+    args = build_parser().parse_args(argv)
+    domains = args.domains or CURATED_DOMAINS
+    for record in fetch_watchlist(
+        domains,
+        crawl_id=args.crawl_id,
+        collinfo_url=args.collinfo_url,
+        page_size=args.page_size,
+        max_pages=args.max_pages,
+        timeout=args.timeout,
+    ):
+        print(json.dumps(record, ensure_ascii=False, separators=(",", ":")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ extract/tests/test_fetch_common_crawl_index.py
@@ -0,0 +1,375 @@
+import contextlib
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import importlib.util
+import io
+import json
+from pathlib import Path
+import threading
+import urllib.error
+import urllib.parse
+
+import pytest
+import yaml
+
+
+SCRIPT = Path(__file__).parents[1] / "scripts" / "fetch_common_crawl_index.py"
+CONFIG = SCRIPT.parents[1] / "sources" / "common_crawl_index.yml"
+SPEC = importlib.util.spec_from_file_location("fetch_common_crawl_index", SCRIPT)
+MODULE = importlib.util.module_from_spec(SPEC)
+assert SPEC.loader is not None
+SPEC.loader.exec_module(MODULE)
+FETCHED_AT = "2026-09-21T12:34:56+00:00"
+
+
+class LocalAPI:
+    def __init__(self):
+        self.requests = []
+        self.responder = None
+
+    def __enter__(self):
+        api = self
+
+        class Handler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                api.requests.append(
+                    {
+                        "path": self.path,
+                        "headers": {key.lower(): value for key, value in self.headers.items()},
+                    }
+                )
+                status, headers, body = api.responder(self.path)
+                if not isinstance(body, bytes):
+                    body = json.dumps(body).encode("utf-8")
+                self.send_response(status)
+                for key, value in headers.items():
+                    self.send_header(key, value)
+                self.send_header("Content-Length", str(len(body)))
+                self.end_headers()
+                self.wfile.write(body)
+
+            def log_message(self, format, *args):
+                pass
+
+        self.server = ThreadingHTTPServer(("127.0.0.1", 0), Handler)
+        self.thread = threading.Thread(target=self.server.serve_forever, daemon=True)
+        self.thread.start()
+        host, port = self.server.server_address
+        self.base_url = f"http://{host}:{port}"
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.server.shutdown()
+        self.server.server_close()
+        self.thread.join(timeout=2)
+
+
+def crawl(crawl_id, index_url):
+    return {"id": crawl_id, "cdx-api": index_url, "name": crawl_id}
+
+
+def cdx_row(number=1, **changes):
+    row = {
+        "urlkey": f"com,example)/page-{number}",
+        "timestamp": f"2026092{number}123456",
+        "url": f"https://example.com/page-{number}",
+        "mime": "text/html",
+        "mime-detected": "text/html",
+        "status": "200",
+        "digest": f"DIGEST{number}",
+        "length": str(1000 + number),
+        "offset": str(2000 + number),
+        "filename": f"crawl-data/CC-MAIN-2026-39/segments/file-{number}.warc.gz",
+        "languages": "eng",
+        "encoding": "UTF-8",
+    }
+    row.update(changes)
+    return row
+
+
+def ndjson(*rows):
+    return b"".join(json.dumps(row).encode("utf-8") + b"\n" for row in rows)
+
+
+def query(path):
+    return urllib.parse.parse_qs(urllib.parse.urlsplit(path).query)
+
+
+def test_discovers_valid_crawls_and_selects_newest_independent_of_metadata_order():
+    with LocalAPI() as api:
+        document = [
+            crawl("CC-MAIN-2026-31", f"{api.base_url}/index/31"),
+            crawl("CC-MAIN-2026-39", f"{api.base_url}/index/39"),
+            crawl("CC-MAIN-2025-51", f"{api.base_url}/index/51"),
+        ]
+        api.responder = lambda path: (200, {"Content-Type": "application/json"}, document)
+
+        crawls = MODULE.discover_crawls(f"{api.base_url}/collinfo.json", timeout=2)
+        selected = MODULE.select_crawl(crawls)
+
+    assert selected == {"id": "CC-MAIN-2026-39", "cdx-api": f"{api.base_url}/index/39"}
+    assert len(api.requests) == 1
+    assert api.requests[0]["path"] == "/collinfo.json"
+    assert api.requests[0]["headers"]["accept"] == "application/json"
+    assert api.requests[0]["headers"]["user-agent"] == MODULE.USER_AGENT
+
+
+def test_explicit_crawl_must_be_present_in_collection_metadata():
+    crawls = [{"id": "CC-MAIN-2026-39", "cdx-api": "https://index.invalid/39"}]
+    assert MODULE.select_crawl(crawls, "CC-MAIN-2026-39") == crawls[0]
+    with pytest.raises(MODULE.CommonCrawlError, match="not present"):
+        MODULE.select_crawl(crawls, "CC-MAIN-2026-31")
+
+
+def test_fetch_domain_paginates_and_normalizes_flat_stable_records():
+    with LocalAPI() as api:
+        def respond(path):
+            values = query(path)
+            if values.get("showNumPages") == ["true"]:
+                return 200, {}, {"pages": 3, "pageSize": 1, "blocks": 3}
+            page = int(values["page"][0])
+            return 200, {"Content-Type": "application/x-ndjson"}, ndjson(cdx_row(page + 1))
+
+        api.responder = respond
+        records = list(
+            MODULE.fetch_domain(
+                "Example.COM.",
+                crawl("CC-MAIN-2026-39", f"{api.base_url}/index"),
+                page_size=1,
+                max_pages=2,
+                timeout=2,
+                fetched_at=FETCHED_AT,
+            )
+        )
+
+    assert len(records) == 2
+    assert len(api.requests) == 3
+    assert [query(item["path"]).get("page") for item in api.requests] == [None, ["0"], ["1"]]
+    for request in api.requests:
+        assert query(request["path"])["url"] == ["example.com/*"]
+        assert query(request["path"])["matchType"] == ["domain"]
+        assert query(request["path"])["output"] == ["json"]
+        assert query(request["path"])["pageSize"] == ["1"]
+
+    first = records[0]
+    assert first == {
+        "source": "common_crawl_index",
+        "id": "CC-MAIN-2026-39:com,example)/page-1:20260921123456",
+        "fetched_at": FETCHED_AT,
+        "crawl_id": "CC-MAIN-2026-39",
+        "watched_domain": "example.com",
+        "url_key": "com,example)/page-1",
+        "timestamp": "20260921123456",
+        "captured_at": "2026-09-21T12:34:56+00:00",
+        "url": "https://example.com/page-1",
+        "mime_type": "text/html",
+        "detected_mime_type": "text/html",
+        "status_code": "200",
+        "digest": "DIGEST1",
+        "content_length": 1001,
+        "warc_offset": 2001,
+        "warc_filename": "crawl-data/CC-MAIN-2026-39/segments/file-1.warc.gz",
+        "languages": "eng",
+        "encoding": "UTF-8",
+    }
+
+
+def test_empty_page_metadata_returns_no_records_without_requesting_data_pages():
+    with LocalAPI() as api:
+        api.responder = lambda path: (200, {}, {"pages": 0, "pageSize": 1, "blocks": 0})
+        records = list(
+            MODULE.fetch_domain(
+                "example.com",
+                crawl("CC-MAIN-2026-39", f"{api.base_url}/index"),
+                timeout=2,
+            )
+        )
+
+    assert records == []
+    assert len(api.requests) == 1
+    assert query(api.requests[0]["path"])["showNumPages"] == ["true"]
+
+
+@pytest.mark.parametrize(
+    "body, message",
+    [
+        ({"id": "CC-MAIN-2026-39"}, "must be a JSON list"),
+        ([{"id": "bad", "cdx-api": "https://index.invalid"}], "invalid id"),
+        ([{"id": "CC-MAIN-2026-39", "cdx-api": 3}], "invalid cdx-api"),
+        (b"not-json", "malformed JSON"),
+    ],
+)
+def test_rejects_malformed_collection_metadata(body, message):
+    with LocalAPI() as api:
+        api.responder = lambda path: (200, {}, body)
+        with pytest.raises(MODULE.CommonCrawlError, match=message):
+            MODULE.discover_crawls(f"{api.base_url}/collinfo", timeout=2)
+
+
+@pytest.mark.parametrize(
+    "metadata, page, message",
+    [
+        ({"pages": "one"}, None, "invalid pages"),
+        ({"pages": 1}, b"not-json\n", "malformed JSON"),
+        ({"pages": 1}, b"[]\n", "must be an object"),
+        ({"pages": 1}, ndjson(cdx_row(timestamp="yesterday")), "invalid timestamp"),
+        ({"pages": 1}, ndjson(cdx_row(length="large")), "must be an integer"),
+    ],
+)
+def test_rejects_malformed_index_responses(metadata, page, message):
+    with LocalAPI() as api:
+        def respond(path):
+            if query(path).get("showNumPages") == ["true"]:
+                return 200, {}, metadata
+            return 200, {}, page
+
+        api.responder = respond
+        with pytest.raises(MODULE.CommonCrawlError, match=message):
+            list(
+                MODULE.fetch_domain(
+                    "example.com",
+                    crawl("CC-MAIN-2026-39", f"{api.base_url}/index"),
+                    timeout=2,
+                )
+            )
+
+
+def test_fetch_domain_propagates_http_errors():
+    with LocalAPI() as api:
+        api.responder = lambda path: (503, {}, b"unavailable")
+        with pytest.raises(urllib.error.HTTPError) as error:
+            list(
+                MODULE.fetch_domain(
+                    "example.com",
+                    crawl("CC-MAIN-2026-39", f"{api.base_url}/index"),
+                    timeout=2,
+                )
+            )
+    assert error.value.code == 503
+
+
+def test_watchlist_resolves_explicit_crawl_once_through_injected_collinfo_url():
+    with LocalAPI() as api:
+        metadata_requests = []
+
+        def respond(path):
+            parsed = urllib.parse.urlsplit(path)
+            if parsed.path == "/fixtures/collinfo.json":
+                metadata_requests.append(path)
+                return 200, {}, [
+                    crawl("CC-MAIN-2026-40", f"{api.base_url}/index/newer"),
+                    crawl("CC-MAIN-2026-39", f"{api.base_url}/index/selected"),
+                ]
+            values = query(path)
+            assert parsed.path == "/index/selected"
+            if values.get("showNumPages") == ["true"]:
+                return 200, {}, {"pages": 1}
+            domain = values["url"][0].removesuffix("/*")
+            return 200, {}, ndjson(
+                cdx_row(1, urlkey=f"com,{domain})/", url=f"https://{domain}/")
+            )
+
+        api.responder = respond
+        records = list(
+            MODULE.fetch_watchlist(
+                ["alpha.com", "beta.com"],
+                crawl_id="CC-MAIN-2026-39",
+                collinfo_url=f"{api.base_url}/fixtures/collinfo.json",
+                max_pages=1,
+                timeout=2,
+                fetched_at=FETCHED_AT,
+            )
+        )
+
+    assert len(metadata_requests) == 1
+    assert [record["watched_domain"] for record in records] == ["alpha.com", "beta.com"]
+    assert {record["crawl_id"] for record in records} == {"CC-MAIN-2026-39"}
+    assert all(request["path"].startswith("/index/selected?") for request in api.requests[1:])
+
+
+def test_watchlist_isolates_domain_http_failure_and_continues(capsys):
+    with LocalAPI() as api:
+        def respond(path):
+            parsed = urllib.parse.urlsplit(path)
+            if parsed.path == "/collinfo":
+                return 200, {}, [crawl("CC-MAIN-2026-39", f"{api.base_url}/index")]
+            values = query(path)
+            domain = values["url"][0]
+            if domain == "broken.example/*":
+                return 500, {}, b"failed"
+            if values.get("showNumPages") == ["true"]:
+                return 200, {}, {"pages": 1}
+            return 200, {}, ndjson(cdx_row())
+
+        api.responder = respond
+        records = list(
+            MODULE.fetch_watchlist(
+                ["broken.example", "working.example"],
+                collinfo_url=f"{api.base_url}/collinfo",
+                timeout=2,
+                fetched_at=FETCHED_AT,
+            )
+        )
+
+    assert len(records) == 1
+    assert records[0]["watched_domain"] == "working.example"
+    error = capsys.readouterr().err
+    assert "skipping 'broken.example'" in error
+    assert "HTTPError" in error
+
+
+def test_main_emits_compact_ndjson_using_injected_collection_endpoint(capsys):
+    with LocalAPI() as api:
+        def respond(path):
+            parsed = urllib.parse.urlsplit(path)
+            if parsed.path == "/collinfo":
+                return 200, {}, [crawl("CC-MAIN-2026-39", f"{api.base_url}/index")]
+            if query(path).get("showNumPages") == ["true"]:
+                return 200, {}, {"pages": 1}
+            return 200, {}, ndjson(cdx_row())
+
+        api.responder = respond
+        MODULE.main(
+            [
+                "--collinfo-url",
+                f"{api.base_url}/collinfo",
+                "--max-pages",
+                "1",
+                "--timeout",
+                "2",
+                "example.com",
+            ]
+        )
+
+    output = capsys.readouterr().out
+    assert output.count("\n") == 1
+    assert json.loads(output)["source"] == MODULE.SOURCE
+    assert ": " not in output
+
+
+def test_source_configuration_matches_bounded_disabled_auto_schema_contract():
+    config = yaml.safe_load(CONFIG.read_text(encoding="utf-8"))
+
+    assert config["name"] == MODULE.SOURCE
+    assert config["script"] == SCRIPT.name
+    assert config["enabled"] is False
+    assert config["sink"] == "local"
+    assert config["format"] == "ndjson"
+    assert config["schema_detection"] == "auto"
+    assert config["keep_payload"] is True
+    assert config["cadence"] == {
+        "auto": True,
+        "floor_minutes": 240,
+        "min_minutes": 240,
+        "max_minutes": 1440,
+        "volatile_keys": ["fetched_at"],
+    }
+    assert tuple(config["curated_domains"]) == MODULE.CURATED_DOMAINS
+    assert config["resource_keys"] == [
+        "common_crawl_index",
+        "common_crawl_api",
+        "extract_sink_resource",
+    ]
+    args = MODULE.build_parser().parse_args(config["args"])
+    assert args.page_size == 1
+    assert args.max_pages == 5
+    assert args.timeout == 30
